e422e0b44e292671f15e39fab238226dab0e615a
Stephen South <stephen.south@caplin.com>
2018-03-29 16:33:09 +0100

    feat(ios): Example bridge included in ios
    Example bridge included in ios and outputted via console.log

--- App.js
+++ App.js
@@ -12,12 +12,18 @@ import {
   View
 } from 'react-native';
 
+import { NativeModules } from 'react-native';
+
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
     'Cmd+D or shake for dev menu',
   android: 'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
 });
+
+for (let name in NativeModules) {
+  console.log("Loaded native module: ", name);
+} 
 
 type Props = {};
 export default class App extends Component<Props> {
